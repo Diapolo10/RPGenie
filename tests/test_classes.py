312990ca86ceac412b707e91dddd35dcb1b61db1
Lari Liuhamo <lari.liuhamo@gmail.com>
2017-09-04 21:55:09 +0300

    Moved tests to root

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,5 +1,9 @@
 #TODO: Write tests compatible with pytest
 
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../src/')
+
 from classes import Item
 from settings import *
 
