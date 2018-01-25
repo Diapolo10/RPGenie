36fba1d039f6d946ca656077f87d64968649415e
Diapolo10 <lari.liuhamo@gmail.com>
2018-01-25 18:36:20 +0200

    Slight improvements to __init__.py, removed cache

--- src/__init__.py
+++ src/__init__.py
@@ -8,7 +8,9 @@ functionality through the pre-defined classes, functions and mixins,
 but you can also directly use any of them as base classes.
 
 The toolkit is aiming to be fully compatible with Python versions 3.6 and up.
-RPGenie is currently being maintained by {}.
+RPGenie is currently being maintained by {maintainer}.
+
+All credit goes to the following people: {credits}
 """
 
 __author__     = "Lari Liuhamo"
@@ -21,4 +23,11 @@ __maintainer__ = __author__
 __email__      = "lari.liuhamo+rpgenie@gmail.com"
 __status__     = "alpha"
 
-__doc__ = __doc__.format(__maintainer__)
+__doc__ = __doc__.format(
+    maintainer = __maintainer__,
+    credits = ", ".join(__credits__)
+        if len(__credits__) > 1
+        else __credits__[0]
+            if __credits__
+            else "None"
+)
